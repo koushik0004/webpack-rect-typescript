8f948a322cb6762d48886ceadf8d118e0ef3dbcc
Koushik Sadhukhan <koushik.sadhukhan0004@gmail.com>
2022-02-04 20:38:43 +0530

    step-4: manage images and svg files webpack config files etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import "./styles.css";
+import IMAGE from "./logo512.png";
+import LOGO from "./logo.svg";
+
 export const App = () => {
   return (
     <>
       <h1>React TypeScript Webpack Starter Template</h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="300" />
+      <img src={LOGO} alt="SVG logo" width="300" />
     </>
   );
 };
---
+++ webpack/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devServer: {
+    // static: {
+    //   directory: path.join(__dirname, 'public'),
+    // },
+    compress: true,
+    port: 9001,
+  },
+  mode: "development",
+  entry: path.resolve(__dirname, "../", "./src/index.tsx"),
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: "asset/inline",
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "../", "./build"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../", "./src/index.html"),
+    }),
+  ],
+};

e322e1a06916dfa5793ce41c980452a163897032
Koushik Sadhukhan <koushik.sadhukhan0004@gmail.com>
2022-02-06 17:53:51 +0530

    step-10: vendr chunks seperation and other optimization

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,39 +1,57 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "../", "./src/index.tsx"),
+  entry: path.resolve(__dirname, '../', './src/index.tsx'),
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }],
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: "asset/inline",
+        type: 'asset/inline',
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: 'all',
+      cacheGroups: {
+        // only for separate vendor file
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
   output: {
-    path: path.resolve(__dirname, "../", "./build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '../', './build'),
+    filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../", "./src/index.html"),
+      template: path.resolve(__dirname, '../', './src/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src', to: 'dest' }],
     }),
   ],
 };
